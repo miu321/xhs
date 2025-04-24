9f8a9efb0cfb3561306dbd77b0fd384d8438a9e9
miu321 <miu321@gmail.com>
2025-04-25 00:12:42 +0100

    upload xhs homepage

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>欢迎来到小红书封面生成器</h1>
+      <p style={{ marginTop: '1rem' }}>这是你的首页项目！</p>
+    </main>
+  );
+}
